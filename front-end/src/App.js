0e99bf255c4a60834cac92ae512c8a0b91e0901e
Omar Malaeb <omarmlb002@gmail.com>
2022-08-06 12:51:10 +0300

    updated app setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from './components/common/header/Header';
 
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Router>
+        <Header />
+        <Switch>
+          {/* <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/program' component={Program} />
+          <Route exact path='/learn' component={Learn} />
+          <Route exact path='/team' component={Team} />
+  <Route exact path='/contact' component={Contact} /> */}
+        </Switch>
+      </Router>
     </div>
   )
 }
