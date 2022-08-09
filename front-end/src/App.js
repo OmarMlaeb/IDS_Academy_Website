6cfe5f5c0461f4a20dabb389a7e3d021a9d0ed99
Omar Malaeb <omarmlb002@gmail.com>
2022-08-10 01:43:30 +0300

    updated errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from './components/common/header/Header.jsx';
 
+import Home from './components/home/Home.jsx';
+
 const App = () => {
   return (
     <div>
       <Router>
         <Header/>
+        <Home/>
         <Switch>
-          {/* <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/program' component={Program} />
-          <Route exact path='/learn' component={Learn} />
-          <Route exact path='/team' component={Team} />
-  <Route exact path='/contact' component={Contact} /> */}
+          <Route path='/' exact component={Home} />
+          {/*<Route path='/about' exact component={About} />
+          <Route path='/program' exact component={Program} />
+          <Route path='/learn' exact component={Learn} />
+          <Route path='/team' exact component={Team} />
+          <Route path='/contact' exact component={Contact} /> */}
         </Switch>
       </Router>
     </div>
--- front-end/src/components/common/header/Header.jsx
+++ front-end/src/components/common/header/Header.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
-import { Link } from "react-router-dom";
-
 import Head from './Head';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>  
             <Head/>
+            <header>
+                <div class="container">
+                    <div class="main-head">
+                        <nav className='nav'>
+                            <ul>
+                                <li> <Link to="/">Home</Link> </li>
+                                <li> <Link to="about">About IDS</Link> </li>
+                                <li> <Link to="program">Internship Program</Link> </li>
+                                <li> <Link to="learn">Learning Hub</Link> </li>
+                                <li> <Link to="team">Meet our Team </Link> </li>
+                                <li> <Link to="contact">Contact Us</Link> </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
         </div>
     )
 }
