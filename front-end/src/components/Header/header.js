364f9b60d9fe66e2a0a425e663148e233ef12ab3
Omar Malaeb <omarmlb002@gmail.com>
2022-08-11 18:58:35 +0300

    updated header

--- front-end/src/components/Header/header.js
+++ front-end/src/components/Header/header.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
+import logo from './../../images/ids_logo.png';
+
 import './header.css';
+import { Link } from 'react-scroll';
 
 const header = () => {
     return (
         <div>
-            <h1>Hi</h1>
+            <div className='head'>
+                <div className='container-head flexSB'>
+                    <div className='logo'>
+                        <Link to='/header'>
+                            <img src={logo} alt="logo"/>
+                        </Link>
+                    </div>
+
+                    <div className='social'>
+                        <i className="fa-brands fa-square-facebook icon"></i>
+                        <i className='fa-brands fa-square-instagram icon'></i>
+                        <i className='fa-brands fa-linkedin icon'></i>
+                    </div>
+                </div>
+            </div>
+            <div className='header'>
+                <nav className='navbar'>
+                    <ul>
+                        <li className='nav-item'>
+                            <a href='/'>Home</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href='#about'>About</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     )
 }
