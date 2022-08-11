e6f040b6037e78e5d06db8244029280ade39ee8d
Omar Malaeb <omarmlb002@gmail.com>
2022-08-11 14:58:26 +0300

    updated header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,6 @@ const App = () => {
     <div>
       <Router>
         <Header/>
-        <Home/>
         <Switch>
           <Route path='/' exact component={Home} />
           {/*<Route path='/about' exact component={About} />
--- front-end/src/components/common/header/Header.jsx
+++ front-end/src/components/common/header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Head from './Head';
 import './header.css';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 const Header = () => {
+
+    const [click, setClick] = useState(false)
+
+    const closeMenu = () => setClick(false)
+
     return (
         <div>  
             <Head/>
@@ -12,13 +17,13 @@ const Header = () => {
                 <div class="container">
                     <div class="main-head">
                         <nav className='nav'>
-                            <ul>
-                                <li> <Link to="/">Home</Link> </li>
-                                <li> <Link to="about">About IDS</Link> </li>
-                                <li> <Link to="program">Internship Program</Link> </li>
-                                <li> <Link to="learn">Learning Hub</Link> </li>
-                                <li> <Link to="team">Meet our Team </Link> </li>
-                                <li> <Link to="contact">Contact Us</Link> </li>
+                            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                                <li className='nav-item'> <Link to="/" spy={true} smooth={true} offset={50} onClick={closeMenu}>Home</Link> </li>
+                                <li className='nav-item'> <Link to="/about" spy={true} smooth={true} offset={50} onClick={closeMenu}>About IDS</Link> </li>
+                                <li className='nav-item'> <Link to="/program" spy={true} smooth={true} offset={50} onClick={closeMenu}>Internship Program</Link> </li>
+                                <li className='nav-item'> <Link to="/learn" spy={true} smooth={true} offset={50} onClick={closeMenu}>Learning Hub</Link> </li>
+                                <li className='nav-item'> <Link to="/team" spy={true} smooth={true} offset={50} onClick={closeMenu}>Meet our Team </Link> </li>
+                                <li className='nav-item'> <Link to="/contact" spy={true} smooth={true} offset={50} onClick={closeMenu}>Contact Us</Link> </li>
                             </ul>
                         </nav>
                     </div>
