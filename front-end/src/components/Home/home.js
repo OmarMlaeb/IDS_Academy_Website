6908c03231dfaefa5ed9c2dab9c98e041448fe36
Omar Malaeb <omarmlb002@gmail.com>
2022-08-15 13:53:21 +0300

    done with home page

--- front-end/src/components/Home/home.js
+++ front-end/src/components/Home/home.js
@@ -11,11 +11,11 @@ const Home = () => {
             <div className='container'>
                 <div className='content_container'>
                     <div className='content'>
-                        <p>New Online</p>
-                        <p>Internship program</p>
-                        <p>August 2022</p>
-                        <p>Long before you sit down to put the pen</p>
-                        <p>need to make sure you breathe</p>
+                        <p className='font1'>New Online</p>
+                        <p className='font1'>Internship program</p>
+                        <p className='font2'>August 2022</p>
+                        <p className='font3'>Long before you sit down to put the pen</p>
+                        <p className='font3'>need to make sure you breathe</p>
                         <button href='/' className='button'>Join Our Program</button>
                     </div>
                 </div>
@@ -28,7 +28,19 @@ const Home = () => {
                 </div>
             </div>
             <div className='about_container'>
-
+                <div className='about_content'>
+                    <p className='font1'>About Us</p>
+                    <p className='font2'>First, a disclaimer - the entire process of writing</p>
+                    <p className='font2'>a blog post often takes more than a couple of</p>
+                    <p className='font2'>hours, even if you can type eighty words as per</p>
+                    <p className='font2'>minute and your writing skills are sharp</p>
+                    <p className='font3'>Requirements</p>
+                    <p className='font4'>- Experience with JavaScript</p>
+                    <p className='font4'>- Good time-management skills</p>
+                    <p className='font4'>- Experience with React</p>
+                    <p className='font4'>- Experience with HTML / CSS</p>
+                    <button href='/' className='button2'>Explore More</button>
+                </div>
             </div>
         </div>
     )
