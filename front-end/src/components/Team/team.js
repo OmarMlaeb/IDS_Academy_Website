6afd9f43ce4ef56c44a456fe782d1c7c1e2551f0
Omar Malaeb <omarmlb002@gmail.com>
2022-08-26 22:14:47 +0300

    done with team page

--- front-end/src/components/Team/team.js
+++ front-end/src/components/Team/team.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import data from "../data";
 
 import './team.css';
 
 const Team = () => {
+
+    const [people] = useState(data);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const lastIndex = people.length - 1;
+        if (index < 0) {
+        setIndex(lastIndex);
+        }
+        if (index > lastIndex) {
+        setIndex(0);
+        }
+    }, [index, people]);
+
+    useEffect(() => {
+        let slider = setInterval(() => {
+        setIndex(index + 1);
+        }, 5000);
+        return () => {
+        clearInterval(slider);
+        };
+    }, [index]);
+
     return (
         <div className='team' id='team'>
-            Team
+            <h1 className='team_heading'><span>meet </span>OUR TEAM</h1>
+            <section className='section'>
+                <div className="section_center">
+                    {people.map((item, indexPeople) => {
+                    const { id, image, name, title, quote } = item;
+                    let position = "nextSlide";
+                    if (indexPeople === index) {
+                        position = "activeSlide";
+                    }
+                    if (
+                        indexPeople === index - 1 ||
+                        (index === 0 && indexPeople === people.length - 1)
+                    ) {
+                        position = "lastSlide";
+                    }
+                    return (
+                        <article className={position} key={id}>
+                        <img src={image} alt={name} className="person_img" />
+                        <h4>{name}</h4>
+                        <p className="title">{title}</p>
+                        <p className="text">{quote}</p>
+                        </article>
+                    );
+                    })}
+                    <button className="prev" onClick={() => setIndex(index - 1)}>
+                        <i className="fa-solid fa-chevron-left"/>
+                    </button>
+                    <button className="next" onClick={() => setIndex(index + 1)}>
+                        <i className="fa-solid fa-chevron-right"/>
+                    </button>
+                </div>
+            </section>
+            <div className='team_container'>
+                <div className='team_content'>
+                    <p className='font1'>Our elite team of technical experts designs and develops apps, websites, and</p>
+                    <p className='font1'>provides solutions for leading companies and governmental institutions.</p>
+                    <p className='font1'>Our team’s expertise allows us to provide advanced solutions on the</p>
+                    <p className='font1'>technological as well as the business levels. Moreover, we cultivate expertise in</p>
+                    <p className='font1'>every stage of product creation and learn from every project to deliver</p>
+                    <p className='font1'>experience.</p>
+                    <p className='font2'>Our team is proudly certified:</p>
+                    <p className='font1'>Project Management Professionals</p>
+                    <p className='font1'>Microsoft</p>
+                    <p className='font1'>Oracle</p>
+                    <p className='font1'>Cisco</p>
+                    <p className='font1'>Scrum</p>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ front-end/src/components/data.js
@@ -0,0 +1,29 @@
+import image1 from './../images/user1.png';
+import image2 from './../images/user2.png';
+import image3 from './../images/user3.png';
+
+const people = [
+    {
+        id: 1,
+        image: image1,
+        name: "Theresa Green",
+        title: "Project Manager",
+        quote: "Teamwork begins by building trust. And the only way to do that is to overcome our need for invulnerability."
+    },
+    {
+        id: 2,
+        image: image2,
+        name: "Laura Norda",
+        title: "UI/UX Designer",
+        quote: "Teamwork begins by building trust. And the only way to do that is to overcome our need for invulnerability."
+    },
+    {
+        id: 3,
+        image: image3,
+        name: "Oscar Nommanee",
+        title: "Web Developer",
+        quote: "No one can whistle a symphony. It takes a whole orchestra to play it."
+    }
+];
+
+export default people;
